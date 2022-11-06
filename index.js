951b1403b2169772788671e6d0356f4a0d86418d
felix kings <felixigwe55@gmail.com>
2022-11-05 18:28:54 -0700

    updated

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-app.use("/api/user", userRoute);
+app.use(express.json());
+ 
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`backend is running`);
